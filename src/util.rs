92291358b0e0a28c8a9376d81cc9cf388b3a6ac3
John T. Wodder II <git@varonathe.org>
2023-06-13 11:31:11 -0400

    Test comma_list

--- src/util.rs
+++ src/util.rs
@@ -4,6 +4,7 @@ pub(crate) fn comma_list<T>(values: &[T]) -> CommaList<'_, T> {
     CommaList(values)
 }
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub(crate) struct CommaList<'a, T>(&'a [T]);
 
 impl<'a, T: fmt::Display> fmt::Display for CommaList<'a, T> {
@@ -19,5 +20,18 @@ impl<'a, T: fmt::Display> fmt::Display for CommaList<'a, T> {
             write!(f, "<none>")?;
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_comma_list() {
+        assert_eq!(comma_list::<u32>(&[]).to_string(), "<none>");
+        assert_eq!(comma_list(&[42]).to_string(), "42");
+        assert_eq!(comma_list(&[42, 23]).to_string(), "42, 23");
+        assert_eq!(comma_list(&[42, 23, 17]).to_string(), "42, 23, 17");
     }
 }
