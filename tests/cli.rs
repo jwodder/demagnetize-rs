e827305c728de9c977c73dd8676b8b8bd17d90d2
John T. Wodder II <git@varonathe.org>
2023-06-17 17:33:21 -0400

    Test getting metadata info that takes more than one piece request

--- tests/cli.rs
+++ tests/cli.rs
@@ -7,7 +7,7 @@ use sha1::{Digest, Sha1};
 use tempfile::tempdir;
 
 #[test]
-fn get_magnet() {
+fn get_magnet_http_udp_trackers() {
     let magnet = concat!(
         "magnet:?xt=urn:btih:fbc325039b4f0e752f2299dca019baadf5095bae",
         "&dn=alpine-standard-3.18.0-aarch64.iso",
@@ -82,5 +82,69 @@ fn get_magnet() {
             "http://fosstorrents.com:6969/announce",
         ]
     );
+    assert!(d.is_empty());
+}
+
+#[test]
+fn get_magnet_multipiece_info() {
+    let magnet = concat!(
+        "magnet:?xt=urn:btih:b851474b74f65cd19f981c723590e3e520242b97",
+        "&dn=debian-12.0.0-amd64-netinst.iso",
+        "&tr=http%3A%2F%2Fbttracker.debian.org%3A6969%2Fannounce",
+        "&ws=https%3A%2F%2Fcdimage.debian.org%2Fcdimage%2Frelease%2F12.0.0%2Famd64%2Fiso-cd%2Fdebian-12.0.0-amd64-netinst.iso",
+        "&ws=https%3A%2F%2Fcdimage.debian.org%2Fcdimage%2Farchive%2F12.0.0%2Famd64%2Fiso-cd%2Fdebian-12.0.0-amd64-netinst.iso",
+    );
+    let tmp_path = tempdir().unwrap();
+    Command::cargo_bin("demagnetize")
+        .unwrap()
+        .arg("--log-level=TRACE")
+        .arg("get")
+        .arg("-o")
+        .arg(tmp_path.path().join("{hash}.torrent"))
+        .arg(magnet)
+        .assert()
+        .success();
+    let path = tmp_path
+        .path()
+        .join("b851474b74f65cd19f981c723590e3e520242b97.torrent");
+    assert!(path.exists());
+    let buf = std::fs::read(path).unwrap();
+    let data = Value::from_bencode(&buf).unwrap();
+    let Value::Dict(mut d) = data else {
+        panic!("Torrent data is not a dict");
+    };
+    let info = d.remove(b"info".as_slice()).unwrap();
+    let Value::Dict(info) = info else {
+        panic!("info is not a dict");
+    };
+    let info_bytes = info.to_bencode().unwrap();
+    let digest = Bytes::from(Sha1::digest(info_bytes).to_vec());
+    assert_eq!(
+        format!("{digest:x}"),
+        "b851474b74f65cd19f981c723590e3e520242b97"
+    );
+    let creation_date = d.remove(b"creation date".as_slice()).unwrap();
+    assert!(matches!(creation_date, Value::Integer(_)));
+    let Value::Bytes(created_by) = d.remove(b"created by".as_slice()).unwrap() else {
+        panic!("'created by' is not a string");
+    };
+    let created_by = std::str::from_utf8(&created_by).unwrap();
+    assert!(created_by.starts_with("demagnetize "));
+    let announce_list = d.remove(b"announce-list".as_slice()).unwrap();
+    let Value::List(lst) = announce_list else {
+        panic!("announce-list is not a list");
+    };
+    let mut trackers = Vec::new();
+    for vals in lst {
+        let Value::List(sublist) = vals else {
+            panic!("Element of announce-list is not a list");
+        };
+        let Value::Bytes(bs) = sublist.into_iter().next().unwrap() else {
+            panic!("Element of element of announce-list is not a string");
+        };
+        let tr = String::from_utf8(bs.into_owned()).unwrap();
+        trackers.push(tr);
+    }
+    assert_eq!(trackers, ["http://bttracker.debian.org:6969/announce"]);
     assert!(d.is_empty());
 }
