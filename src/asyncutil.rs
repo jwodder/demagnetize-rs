8fd604d2168faa13ef9df411fa46d2bb0763e735
John T. Wodder II <git@varonathe.org>
2023-06-17 13:33:44 -0400

    ShutdownGroup

--- src/asyncutil.rs
+++ src/asyncutil.rs
@@ -1,13 +1,17 @@
 use futures::future::{maybe_done, MaybeDone};
 use futures::ready;
-use futures::stream::Stream;
+use futures::stream::{FuturesUnordered, Stream, StreamExt};
 use pin_project_lite::pin_project;
 use std::collections::HashSet;
 use std::future::Future;
 use std::hash::Hash;
 use std::pin::Pin;
 use std::task::{Context, Poll};
+use std::time::Duration;
 use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tokio::task::JoinHandle;
+use tokio::time::timeout;
+use tokio_util::sync::CancellationToken;
 
 /// `received_stream()` takes a buffer size and an async procedure that takes a
 /// [`tokio::sync::mpsc::Sender`], and it returns a stream that runs the
@@ -123,10 +127,45 @@ where
     }
 }
 
+// TODO: Should there be a limit on the number of tasks running at once?
+pub(crate) struct ShutdownGroup {
+    handles: FuturesUnordered<JoinHandle<()>>,
+    token: CancellationToken,
+}
+
+impl ShutdownGroup {
+    pub(crate) fn new() -> Self {
+        ShutdownGroup {
+            handles: FuturesUnordered::new(),
+            token: CancellationToken::new(),
+        }
+    }
+
+    pub(crate) fn spawn<F, Fut>(&self, func: F)
+    where
+        F: FnOnce(CancellationToken) -> Fut,
+        Fut: Future<Output = ()> + Send + 'static,
+    {
+        let future = func(self.token.clone());
+        self.handles.push(tokio::spawn(future));
+    }
+
+    async fn join(&mut self) {
+        while self.handles.next().await.is_some() {}
+    }
+
+    pub(crate) async fn shutdown(mut self, duration: Duration) {
+        if timeout(duration, self.join()).await.is_err() {
+            self.token.cancel();
+        }
+        self.join().await;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use futures::stream::{iter, StreamExt};
+    use futures::stream::iter;
     use std::io::Cursor;
     use std::sync::atomic::{AtomicBool, Ordering};
     use std::sync::Arc;
@@ -190,5 +229,29 @@ mod tests {
         let stream = iter([10, 20, 30, 20, 40, 10, 50]).unique();
         tokio::pin!(stream);
         assert_eq!(stream.collect::<Vec<_>>().await, vec![10, 20, 30, 40, 50]);
+    }
+
+    #[tokio::test]
+    async fn test_shutdown_group() {
+        let group = ShutdownGroup::new();
+        let task1_finished = Arc::new(AtomicBool::new(false));
+        let my_finished = task1_finished.clone();
+        group.spawn(|token| async move {
+            tokio::select! {
+                _ = token.cancelled() => (),
+                _ = futures::future::ready(()) => my_finished.store(true, Ordering::Release),
+            }
+        });
+        let task2_cancelled = Arc::new(AtomicBool::new(false));
+        let my_cancelled = task2_cancelled.clone();
+        group.spawn(|token| async move {
+            tokio::select! {
+                _ = token.cancelled() => my_cancelled.store(true, Ordering::Release),
+                _ = tokio::time::sleep(Duration::from_secs(10)) => (),
+            }
+        });
+        group.shutdown(Duration::from_secs(1)).await;
+        assert!(task1_finished.load(Ordering::Acquire));
+        assert!(task2_cancelled.load(Ordering::Acquire));
     }
 }
